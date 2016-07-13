7974ec7bc7b5699d3b06645ea2f7a0ccc136d968
pvdwijdeven <pvdwijdeven@gmail.com>
2016-07-13 11:54:36 +0200

    final bridge edges

--- Debug/Debug.py
+++ Debug/Debug.py
@@ -1,17 +1,42 @@
 debug = 1 + 2 + 4 + 8 + 16 + 32 + 64 + 128 + 256 + 512 + 1024
 
+
+class DColors:
+    def __init__(self):
+        pass
+
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    NORMAL = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def dprint(level=0, color=DColors.NORMAL, *arg):
+    if debug & level or level == -1:
+        print color + ' '.join(map(str, arg)) + DColors.NORMAL  #
+
+
 for x in xrange(10):
     if debug & 2 ** x:
         print "Debug level:", x + 1, "is active"
+    dprint(2 ** x, DColors.BLUE, "Debug printing level:", x + 1, "is active")
 
 # example:
 debug = 4
 
 if debug & 2:
     print "Debug level 2 active"  # not active
+dprint(2, DColors.GREEN, "Debug level 2 printing is active")
 if debug & 4:
     print "Debug level 3 active"  # only this one is active now
+dprint(4, DColors.GREEN, "Debug level 3 printing is active")
 if debug & 8:
     print "Debug level 4 active"  # not active
+dprint(8, DColors.GREEN, "Debug level 4 printing is active")
 if debug:
-    print "Debug active"  # active if debug > 0
+    print "Debug active"  # active if debug != 0
+dprint(-1, DColors.GREEN, "Debug printing is active")
--- Graph/BridgeEdges.py
+++ Graph/BridgeEdges.py
@@ -1,35 +1,67 @@
-#
-# First some utility functions
+# first set debug level
+# -1 = general debug when debug != 0
+# 1 = create spanning tree
+# 2 = post order
+# 4 = number of descendants
+# 8 = lowest post order
+# 16 = highest post order
+# 32 = bridge edges
+# 64 = test cases
+# 128 = main test case
+
+debug = 1 + 2 + 4 + 8 + 16 + 32 + 128
+
+
+class DColors:
+    def __init__(self):
+        pass
+
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    NORMAL = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def dprint(level=0, color=DColors.NORMAL, *arg):
+    if debug & level or level == -1:
+        print color + ' '.join(map(str, arg)) + DColors.NORMAL  #
+
+
+# Now some utility functions
 #
 
 
-def make_link(G, node1, node2, r_or_g):
+def make_link(graph, node1, node2, r_or_g):
     # modified make_link to apply
     # a color to the edge instead of just 1
-    if node1 not in G:
-        G[node1] = {}
-    (G[node1])[node2] = r_or_g
-    if node2 not in G:
-        G[node2] = {}
-    (G[node2])[node1] = r_or_g
-    return G
+    if node1 not in graph:
+        graph[node1] = {}
+    (graph[node1])[node2] = r_or_g
+    if node2 not in graph:
+        graph[node2] = {}
+    (graph[node2])[node1] = r_or_g
+    return graph
 
 
-def get_children(S, root, parent):
+def get_children(spanning_tree, root, parent):
     """returns the children from following the
     green edges"""
-    return [n for n, e in S[root].items()
+    return [n for n, e in spanning_tree[root].items()
             if ((not n == parent) and
                 (e == 'green'))]
 
 
-def get_children_all(S, root, parent):
+def get_children_all(spanning_tree, root, parent):
     """returns the children from following
     green edges and the children from following
     red edges"""
     green = []
     red = []
-    for n, e in S[root].items():
+    for n, e in spanning_tree[root].items():
         if n == parent:
             continue
         if e == 'green':
@@ -41,67 +73,76 @@ def get_children_all(S, root, parent):
 
 #################
 
-def create_rooted_spanning_tree(G, root):
+def create_rooted_spanning_tree(graph, root):
     # use DFS from the root to add edges and nodes
     # to the tree.  The first time we see a node
     # the edge is green, but after that its red
-    open_list = [root]
-    S = {root: {}}
-    while len(open_list) > 0:
-        node = open_list.pop()
-        neighbors = G[node]
+    dprint(1, DColors.BLUE, "start create_rooted_spanning_tree:")
+    todo_list = [root]
+    spanning_tree = {root: {}}
+    while todo_list:
+        node = todo_list.pop()
+        dprint(1, DColors.NORMAL, "node:", node, "open_list:", todo_list)
+        neighbors = graph[node]
         for n in neighbors:
-            if n not in S:
+            if n not in spanning_tree:
                 # we haven't seen this node, so
                 # need to use a green edge to connect
                 # it
-                make_link(S, node, n, 'green')
-                open_list.append(n)
+                make_link(spanning_tree, node, n, 'green')
+                todo_list.append(n)
             else:
                 # we have seen this node,
                 # but, first make sure that
                 # don't already have the edge
-                # in S
-                if node not in S[n]:
-                    make_link(S, node, n, 'red')
-    return S
+                # in spanning_tree
+                if node not in spanning_tree[n]:
+                    make_link(spanning_tree, node, n, 'red')
+    dprint(1, DColors.BLUE, "done create_rooted_spanning_tree:\n", spanning_tree)
+    return spanning_tree
 
 
 ##################
 
-def _post_order(S, root, parent, val, po):
-    children = get_children(S, root, parent)
+def _post_order(spanning_tree, root, parent, val, po):
+    children = get_children(spanning_tree, root, parent)
     for c in children:
-        val = _post_order(S, c, root, val, po)
+        val = _post_order(spanning_tree, c, root, val, po)
+        dprint(2, DColors.NORMAL, "node:", root, "child:", c, "value:", val)
     po[root] = val
     return val + 1
 
 
-def post_order(S, root):
+def post_order(spanning_tree, root):
+    dprint(2, DColors.BLUE, "start post_order:")
     po = {}
-    _post_order(S, root, None, 1, po)
+    _post_order(spanning_tree, root, None, 1, po)
+    dprint(2, DColors.BLUE, "done post_order:\n", po)
     return po
 
 
 ##################
 
-def _number_descendants(S, root, parent, nd):
+def _number_descendants(spanning_tree, root, parent, nd):
     # number of descendants is the
     # sum of the number of descendants of a nodes
     # children plus one
-    children = get_children(S, root, parent)
+    children = get_children(spanning_tree, root, parent)
     nd_val = 1
     for c in children:
         # recursively calculate the number of descendants
         # for the children
-        nd_val += _number_descendants(S, c, root, nd)
+        nd_val += _number_descendants(spanning_tree, c, root, nd)
     nd[root] = nd_val
+    dprint(4, DColors.NORMAL, "node:", root, "value:", nd_val)
     return nd_val
 
 
-def number_of_descendants(S, root):
+def number_of_descendants(spanning_tree, root):
+    dprint(4, DColors.BLUE, "start number_of_descendants:")
     nd = {}
-    _number_descendants(S, root, None, nd)
+    _number_descendants(spanning_tree, root, None, nd)
+    dprint(4, DColors.BLUE, "end number_of_descendants:", nd)
     return nd
 
 
@@ -110,12 +151,12 @@ def number_of_descendants(S, root):
 # a similar method, I only wrote one method
 # that can be used for both
 #
-def _general_post_order(S, root, parent, po, gpo, comp):
-    green, red = get_children_all(S, root, parent)
+def _general_post_order(spanning_tree, root, parent, po, gpo, comp):
+    green, red = get_children_all(spanning_tree, root, parent)
     val = po[root]
     for c in green:
         # recursively find the low/high post order value of the children
-        test = _general_post_order(S, c, root, po, gpo, comp)
+        test = _general_post_order(spanning_tree, c, root, po, gpo, comp)
         # and save the low/highest one
         if comp(val, test):
             val = test
@@ -127,18 +168,23 @@ def _general_post_order(S, root, parent, po, gpo, comp):
         if comp(val, test):
             val = test
     gpo[root] = val
+    dprint(8 + 16, DColors.NORMAL, "root:", root, "value:", val)
     return val
 
 
-def lowest_post_order(S, root, po):
+def lowest_post_order(spanning_tree, root, po):
+    dprint(8, DColors.BLUE, "start lowest_post_order:")
     lpo = {}
-    _general_post_order(S, root, None, po, lpo, lambda x, y: x > y)
+    _general_post_order(spanning_tree, root, None, po, lpo, lambda x, y: x > y)
+    dprint(8, DColors.BLUE, "end lowest_post_order:\n", lpo)
     return lpo
 
 
-def highest_post_order(S, root, po):
+def highest_post_order(spanning_tree, root, po):
+    dprint(16, DColors.BLUE, "start highest_post_order:")
     hpo = {}
-    _general_post_order(S, root, None, po, hpo, lambda x, y: x < y)
+    _general_post_order(spanning_tree, root, None, po, hpo, lambda x, y: x < y)
+    dprint(16, DColors.BLUE, "end highest_post_order:\n", hpo)
     return hpo
 
 
@@ -146,25 +192,30 @@ def highest_post_order(S, root, po):
 # Now put everything together
 #
 
-def bridge_edges(G, root):
-    S = create_rooted_spanning_tree(G, root)
-    po = post_order(S, root)
-    nd = number_of_descendants(S, root)
-    lpo = lowest_post_order(S, root, po)
-    hpo = highest_post_order(S, root, po)
+def bridge_edges(graph, root):
+    dprint(32, DColors.BLUE, "start bridge_edges:")
+    spanning_tree = create_rooted_spanning_tree(graph, root)
+    po = post_order(spanning_tree, root)
+    nd = number_of_descendants(spanning_tree, root)
+    lpo = lowest_post_order(spanning_tree, root, po)
+    hpo = highest_post_order(spanning_tree, root, po)
     bridges = []
     open_list = [(root, None)]
     # walk down the tree and see which edges are
     # tree edges
     while len(open_list) > 0:
         node, parent = open_list.pop()
-        for child in get_children(S, node, parent):
+        for child in get_children(spanning_tree, node, parent):
             # all of these edges are automatically green (get_children only
             # follows green edges)
             # so only need to check the other two conditions
+            dprint(32, DColors.NORMAL, "child:", child, "po:", po[child], "hpo", hpo[child], "lpo:", lpo[child], "nd:",
+                   nd[child], "bridge:",
+                   hpo[child] <= po[child] and lpo[child] > (po[child] - nd[child]))
             if hpo[child] <= po[child] and lpo[child] > (po[child] - nd[child]):
                 bridges.append((node, child))
             open_list.append((child, node))
+    dprint(32, DColors.BLUE, "end bridge_edges:\n", bridges)
     return bridges
 
 
@@ -257,9 +308,17 @@ def test_bridge_edges():
     assert bridges == [('d', 'e')]
 
 
-test_create_rooted_spanning_tree()
-test_post_order()
-test_number_of_descendants()
-test_lowest_post_order()
-test_highest_post_order()
-test_bridge_edges()
+if debug & 64:
+    dprint(64, DColors.GREEN, "\n***** testing create rooted spanning tree *****")
+    test_create_rooted_spanning_tree()
+    dprint(64, DColors.GREEN, "\n***** testing post order *****")
+    test_post_order()
+    dprint(64, DColors.GREEN, "\n***** testing number of descendants *****")
+    test_number_of_descendants()
+    dprint(64, DColors.GREEN, "\n***** testing lowest post order *****")
+    test_lowest_post_order()
+    dprint(64, DColors.GREEN, "\n***** testing highest post order *****")
+    test_highest_post_order()
+if debug & 128:
+    dprint(128, DColors.GREEN, "\n***** testing bridge edges *****")
+    test_bridge_edges()
