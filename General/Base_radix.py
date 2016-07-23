0104086253f6abf022e600b81c694cd1214ac271
pvdwijdeven <pvdwijdeven@gmail.com>
2016-07-23 08:40:23 +0200

    updated primes

---
+++ General/Base_radix.py
@@ -0,0 +1,9 @@
+def baseN(num,b,numerals="0123456789abcdefghijklmnopqrstuvwxyz"):
+    return ((num == 0) and numerals[0]) or (baseN(num // b, b, numerals).lstrip(numerals[0]) + numerals[num % b])
+	
+x=123456789
+
+y=baseN(x,16)
+print y
+
+print int(y,16)
--- General/Primes.py
+++ General/Primes.py
@@ -20,6 +20,15 @@ def get_all_primes_to_limit(limit):
     return primes_
 
 
+def primes(n):
+    """ Returns  a list of primes < n """
+    sieve = [True] * n
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+    return [2] + [i for i in xrange(3,n,2) if sieve[i]]
+	
+
 def main():
     primes = get_all_primes_to_limit(10000)
     print primes
